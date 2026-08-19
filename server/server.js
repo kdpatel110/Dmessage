28e9f3b880eb5c35ca9f03f3d7bfefa687625d63
Dhruvi Kalariya <dhruvikalariya011@gmail.com>
2026-08-19 14:17:11 +0530

    intial commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import 'dotenv/config'
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+console.log(port);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
