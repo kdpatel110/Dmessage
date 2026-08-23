033390529fa156329111230bdfc0c44b2a4c131b
Dhruvi Kalariya <dhruvikalariya011@gmail.com>
2026-08-23 22:08:56 +0530

    static file: i am actully confused, let's see

--- server/server.js
+++ server/server.js
@@ -4,12 +4,16 @@ import cors from 'cors'
 import { connectDB } from './lib/db.js'
 import User from './models/user.model.js'
 import { clerkMiddleware } from '@clerk/express'
+import fs from 'fs';
+import path from 'path';
 
 
 const app = express()
 
 const port = process.env.PORT || 3000;
 const FRONTEND_URL = process.env.FRONTEND_URL
+
+const publicDir = path.join(process.cwd(), "public");
 
 app.use(express.json());
 app.use(cors({
@@ -21,6 +25,14 @@ app.use(clerkMiddleware());
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+if(fs.existsSync(publicDir)){
+  app.use(express.static(publicDir))
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  })
+}
 
 app.listen(port, () => {
   connectDB();
