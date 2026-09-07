9a360c9dfcc40a0eb0cd35721ae9429cdc70da20
Dhruvi Kalariya <dhruvikalariya011@gmail.com>
2026-09-07 16:52:41 +0530

    auth route and middleware

---
+++ server/middleware/auth.middleware.js
@@ -0,0 +1,23 @@
+import { getAuth } from "@clerk/express";
+import User from "../models/user.model";
+
+export async function protectRoute(req, res, next) {
+    try {
+        const { userId } = getAuth(req);
+        if (!userId) {
+            res.status(401).json({ message: "Unauthorized" });
+            return;
+        }
+
+        const user = await User.findOne({ clerkId: userId });
+        if (!user) {
+            res.status(404).json({ message: "user profile is not synced yet" });
+            return;
+        }
+
+        req.user = user
+        next()
+    } catch (error) {
+        console.error("Error in protectRout middleware", error.message);
+    }
+}
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import fs from 'fs';
 import path from 'path';
 import job from './lib/cron.js'
 import clerkWebhook from './webhooks/clerk.webhook.js'
-
+import authRoutes from './routes/auth.route.js'
 
 const app = express()
 
@@ -27,10 +27,11 @@ app.use(cors({
 }));
 app.use(clerkMiddleware());
 
-
 app.get("/health", (req, res)=>{
   res.status(200).json({ok: true});
 })
+
+app.use("/api/auth", authRoutes)
 
 if(fs.existsSync(publicDir)){
   app.use(express.static(publicDir))
