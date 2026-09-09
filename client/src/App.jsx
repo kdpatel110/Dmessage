caefe8de12f45efcdde51e304555763bfb756527
Dhruvi Kalariya <dhruvikalariya011@gmail.com>
2026-09-09 10:47:17 +0530

    impliment Auth Store

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,19 +6,37 @@ import { Navigate, Route, Routes } from 'react-router'
 import ChatPage from './pages/ChatPage'
 import AuthPage from './pages/AuthPage'
 import PageLoader from './components/PageLoader'
+import { useAuthStore } from './store/useAuthStore'
+import {Toaster} from 'react-hot-toast'
 
 function App() {
   const { isSignedIn, isLoaded } = useAuth();
 
-  if(!isLoaded) return <PageLoader/>
+  // option 1
+  // const { checkAuth, isCheckingAuth, clearAuth } = useAuthStore();
+
+  // option 2 - better for performance
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    if (isSignedIn) checkAuth();
+    else clearAuth();
+  }, [checkAuth, clearAuth, isLoaded, isSignedIn]);
+
+  if (!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader />;
 
   return (
     <ThemeProvider>
       <WallpaperProvider>
         <Routes>
-          <Route path='/' element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace/>} />
-          <Route path='/auth' element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace/>} />
+          <Route path='/' element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
+          <Route path='/auth' element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />} />
         </Routes>
+        <Toaster/>
       </WallpaperProvider>
     </ThemeProvider>
   )
--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -1,28 +1,54 @@
 import React from 'react'
 import { create } from "zustand"
 import { axiosInstance } from '../lib/axios';
+import { io } from 'socket.io-client'
 
-export const useAuthStore = create((set, get) => ({
+const BASE_URL = import.meta.env.MODE === "devlopment" ? "http://localhost:3000" : "/";
+
+//(set, get) two useful function
+//set use to change the state
+//
+export const useAuthStore = create((set, get) => ({ //global authentication store
+    //global variables
     authUser: null,
-    isChekingAuth: true,
-    onlineUsers: [],
-    socket: null,
+    isCheckingAuth: true,
+    onlineUsers: [], //beign updated using socket.io code
+    socket: null, //this store socket.io connection
 
+    //Its job: Ask your backend: "Is this user authenticated?
     checkAuth: async () => {
-        set({ isChekingAuth: true });
+        set({ isCheckingAuth: true });
 
         try {
             const res = await axiosInstance.get("/auth/check")
             set({ authUser: res.data })
+
+            get().connectSocket(res.data);
         } catch (error) {
             console.error("Error in checkAuth:", error);
             set({ authUser: null });
         } finally {
-            set({ isChekingAuth: false });
+            set({ isCheckingAuth: false });
         }
     },
+    //logout cleanup
     clearAuth: () => {
         set({ authUser: null, isCheckingAuth: false, onlineUsers: [] });
         get().disconnectSocket();
+    },
+
+    connectSocket: (user) => {
+        if (!user || get().socket?.connected) return
+        const socket = io(BASE_URL, { query: { userId: user._id } })
+        set({ socket })
+        socket.on("getOnlineUsers", (userIds) => {
+            set({ onlineUsers: userIds });
+        })
+    },
+
+    disconnectSocket: () => {
+        const socket = get().socket;
+        if (socket?.connected) socket.disconnect();
+        set({ socket: null });
     },
 }))
