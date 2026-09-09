ba5c6d68e1ab2c1dba59d219d3c9e889085cef07
Dhruvi Kalariya <dhruvikalariya011@gmail.com>
2026-09-09 09:01:26 +0530

    axios

---
+++ client/src/lib/axios.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+export const axiosInstance = axios.create({
+    baseURL: import.meta.env.MODE === "devlopment" ? "http://localhost:3000/api" : "/api",
+    withCredentials: true, //means include cookies and header 
+});
